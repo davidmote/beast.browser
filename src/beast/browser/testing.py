2ac48d89e0e0415d96bae2131098da42ebdba0db
Marco A. Martinez <mam002@ucsd.edu>
2011-10-05 11:34:07 -0700

    Changes accepted with some modifications applied

--- src/beast/browser/testing.py
+++ src/beast/browser/testing.py
@@ -7,14 +7,20 @@ from plone.testing import z2
 
 from zope.configuration import xmlconfig
 
-class beastBrowserPolicy(PloneSandboxLayer):
+
+class BeastBrowserPolicy(PloneSandboxLayer):
 
     defaultBases = (PLONE_FIXTURE,)
-    
+
     def setUpZope(self, app, configurationContext):
         # Load ZCML
-        import beast.browser
-        xmlconfig.file('configure.zcml', beast.browser, context=configurationContext)
+        import beast.browser as package
+        xmlconfig.file('configure.zcml', package, context=configurationContext)
 
-BEASTBROWSER_POLICY_FIXTURE = beastBrowserPolicy()
-BEASTBROWSER_POLICY_INTEGRATION_TESTING = IntegrationTesting(bases=(BEASTBROWSER_POLICY_FIXTURE,), name="beastbrowser:Integration")
+
+BEAST_BROWSER_FIXTURE = BeastBrowserPolicy()
+
+BEAST_BROWSER_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(BEAST_BROWSER_FIXTURE,),
+    name='BeastBrowser:Integration'
+    )
